530592deab3cf186648f10454e0c097e31810e84
Shaamala <shaamalajabbar@gmail.com>
2018-01-23 13:39:28 -0500

    first commit helloworld

---
+++ src/GitHelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * Abduljabbar Shaammal
+ * 
+ * This sample hello world program that will used to push to github
+ */
+public class GitHelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!");
+	}
+
+}
